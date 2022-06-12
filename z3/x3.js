13e3c7ae7be32ab0c77d6cccb8a06ee7a5e53b24
20Erik <93816971+20Erik@users.noreply.github.com>
2022-06-12 13:46:52 +0300

    Add files via upload

---
+++ z3/x3.js
@@ -0,0 +1,25 @@
+﻿/*alert(`Привіт`);
+
+let ne =`Діма` ;
+let
+let n = prompt(`як звати президента України`);
+if(neme=`Володимир`){
+	alert(`Правда`)
+}else{
+	alert(`Неправда`)
+}
+let age = prompt(`яка ціна картоплі ? `);
+if(age=18<17){
+	alert(`дорого`)
+}else{
+	alert(`дешево`)
+}*/
+alert(`вітаю на сайті MovielList`);
+let n = prompt(`як вас звати`);
+alert(`вітаю ${n}`)
+let a = confirm(` вам є  18 років`)
+if(a==true){
+	alert(` вартісьть квитка 100 грн `)
+}else{
+	alert(`вартісьть квитка 70 грн`)
+}
